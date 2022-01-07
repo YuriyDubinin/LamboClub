0b80163b88c5494554e797d9626e0f211f22e82c
Yura Dubinin <yuradubinin100@gmail.com>
2022-01-07 17:45:31 +0700

    added functionality for sending data from a form to the server in the FormData format

--- js/script.js
+++ js/script.js
@@ -4,3 +4,33 @@ $(document).ready(function () {
         $("body").toggleClass("lock");
     });
 });
+
+/* forms */
+//declaration
+const forms = document.querySelectorAll("form");
+
+//responsible to the sending data from form to the server
+function postData(form) {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        const request = new XMLHttpRequest();
+        const formData = new FormData(form);
+
+        request.open("POST", "server.php");
+        request.send(formData);
+
+        request.addEventListener("load", () => {
+            if (request.status === 200) {
+                console.log(request.response);
+            } else {
+                console.log("error: something is wrong..");
+            }
+        });
+    });
+}
+
+//execution
+forms.forEach((item) => {
+    postData(item);
+});
